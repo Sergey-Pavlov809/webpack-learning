12f9b77dcd82605ea5c1468393af87c2220f04c6
sergey_pavlov <pavlitto228@mail.ru>
2021-04-19 01:57:20 +0300

    доделал соурс мап и babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports={
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'development',
+    devtool: 'source-map',
     devServer: {
         historyApiFallback: true
     },
